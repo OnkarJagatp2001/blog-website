c314cf50baa7a068f7c9660fa6f6b2450963a147
Yogesh Yenage <yengeyogesh3951@gmail.com>
2023-07-02 18:41:03 +0530

    home component all cleared

--- front-end/src/app/components/home-page/home-page.component.ts
+++ front-end/src/app/components/home-page/home-page.component.ts
@@ -5,19 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent implements OnInit{
-  ngOnInit(): void {
-    // this.isUserLoggedIn();
-  }
-  isUserLoggedIn()
-  {
-    let vl = sessionStorage.getItem('userid')
-    let vs = parseInt(vl || '')
-    
-    if(Number.isNaN(vs))
-    return false;
-    else
-    return true;
-  }
-  isLoggedIn:boolean=this.isUserLoggedIn();
+export class HomePageComponent {
+  
 }
